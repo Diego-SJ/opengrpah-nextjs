01f143f577cac7b4a0701b87d69e9324f1cd80a0
diego <diego@MBPPersdeMartin.bwrouter>
2022-04-22 03:20:07 -0500

    _document updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,38 +7,54 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en" dir="ltr">
 				<Head>
+					{/* ================== Links and Metas ================== */}
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+						rel="stylesheet"
+					/>
 					<meta charSet="utf-8" />
-					<meta name="description" content={'SITE_DESCRIPTION'} />
-					<meta name="author" content={'SITE_AUTHOR'} />
-					<link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon" />
-					<link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
-					{/* PWA primary color */}
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<meta name="facebook-domain-verification" content="409ftxgnl2ibsi2lit6lkeu2r5bgvn" />
+
+					<meta
+						property="og:title"
+						content="Zenda.la | El plan de salud y prevención que responde por ti"
+					/>
+					<meta
+						name="twitter:title"
+						content="Zenda.la | El plan de salud y prevención que responde por ti"
+					/>
+					<meta property="og:url" content="https://zenda.la/" />
 					<meta property="og:type" content="website" />
-					<meta property="og:site_name" content={'SITE_NAME'} />
-					<meta property="og:title" content={'TESTO OPEN GRPAH DOCUMENT'} />
-					<meta property="og:description" content={'SITE_DESCRIPTION'} />
+					<meta
+						name="description"
+						content="Creamos los planes de protección ideales para tus necesidades y presupuesto. Elige tu plan..."
+					/>
+					<meta
+						property="og:description"
+						content="Creamos los planes de protección ideales para tus necesidades y presupuesto. Elige tu plan..."
+					/>
+					<meta
+						name="twitter:description"
+						content="Creamos los planes de protección ideales para tus necesidades y presupuesto. Elige tu plan..."
+					/>
 					<meta
 						property="og:image"
-						content={
-							'https://firebasestorage.googleapis.com/v0/b/zenda-186015.appspot.com/o/cms%2Fzendala%20landscape.png?alt=media&token=af50885a-7b33-474f-8a5f-15a114cf55f6'
-						}
+						content="https://firebasestorage.googleapis.com/v0/b/zenda-186015.appspot.com/o/cms%2Fzendala%20landscape.png?alt=media&token=af50885a-7b33-474f-8a5f-15a114cf55f6"
 					/>
-					<meta name="twitter:card" content="summary_large_image" />
-					<meta name="twitter:site" content={'SITE_NAME'} />
-					<meta name="twitter:title" content={'TESTO OPEN GRPAH DOCUMENT'} />
-					<meta name="twitter:description" content={'SITE_DESCRIPTION'} />
 					<meta
-						property="twitter:image"
-						content={
-							'https://firebasestorage.googleapis.com/v0/b/zenda-186015.appspot.com/o/cms%2Fzendala%20landscape.png?alt=media&token=af50885a-7b33-474f-8a5f-15a114cf55f6'
-						}
+						name="twitter:image"
+						content="https://firebasestorage.googleapis.com/v0/b/zenda-186015.appspot.com/o/cms%2Fzendala%20landscape.png?alt=media&token=af50885a-7b33-474f-8a5f-15a114cf55f6"
 					/>
-					<link
-						rel="stylesheet"
-						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+					<meta
+						name="image"
+						content="https://firebasestorage.googleapis.com/v0/b/zenda-186015.appspot.com/o/cms%2Fzendala%20landscape.png?alt=media&token=af50885a-7b33-474f-8a5f-15a114cf55f6"
 					/>
-					<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-					<link rel="stylesheet" href="/static/styles/main.css" />
+					<meta
+						name="keywords"
+						content="aseguradora, seguros en línea, seguros online, seguro de auto, hospitalización, seguro médico"
+					/>
 				</Head>
 				<body>
 					<Main />
