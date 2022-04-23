ea8a16b0fc3c1c940b7d80a6991a5eefe5ed3f5c
diego <diego@MBPPersdeMartin.bwrouter>
2022-04-23 14:23:03 -0500

    remove  app context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import Head from 'next/head';
-import GlobalThemeProvider from '../theme/theme';
+// import GlobalThemeProvider from '../theme/theme';
 import App, { AppContext, AppProps } from 'next/app';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import store, { persistor } from '../redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { Provider } from 'react-redux';
+// import store, { persistor } from '../redux/store';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return (
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<GlobalThemeProvider>
-					<Head>
-						<title>Next-Smrt</title>
-						<meta
-							name="viewport"
-							content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-						/>
-					</Head>
-					<Component {...pageProps} />
-				</GlobalThemeProvider>
-			</PersistGate>
-		</Provider>
+		<>
+			<Head>
+				<title>Next-Smrt</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
 	);
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,56 +7,7 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en" dir="ltr">
-				<Head>
-					{/* ================== Links and Metas ================== */}
-					<link rel="preconnect" href="https://fonts.gstatic.com" />
-					<link
-						href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
-						rel="stylesheet"
-					/>
-					<meta charSet="utf-8" />
-					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-					<meta name="facebook-domain-verification" content="409ftxgnl2ibsi2lit6lkeu2r5bgvn" />
-
-					<meta
-						property="og:title"
-						content="Zenda.la | El plan de salud y prevención que responde por ti"
-					/>
-					<meta
-						name="twitter:title"
-						content="Zenda.la | El plan de salud y prevención que responde por ti"
-					/>
-					<meta property="og:url" content="https://zenda.la/" />
-					<meta property="og:type" content="website" />
-					<meta
-						name="description"
-						content="Creamos los planes de protección ideales para tus necesidades y presupuesto. Elige tu plan..."
-					/>
-					<meta
-						property="og:description"
-						content="Creamos los planes de protección ideales para tus necesidades y presupuesto. Elige tu plan..."
-					/>
-					<meta
-						name="twitter:description"
-						content="Creamos los planes de protección ideales para tus necesidades y presupuesto. Elige tu plan..."
-					/>
-					<meta
-						property="og:image"
-						content="https://firebasestorage.googleapis.com/v0/b/zenda-186015.appspot.com/o/cms%2Fzendala%20landscape.png?alt=media&token=af50885a-7b33-474f-8a5f-15a114cf55f6"
-					/>
-					<meta
-						name="twitter:image"
-						content="https://firebasestorage.googleapis.com/v0/b/zenda-186015.appspot.com/o/cms%2Fzendala%20landscape.png?alt=media&token=af50885a-7b33-474f-8a5f-15a114cf55f6"
-					/>
-					<meta
-						name="image"
-						content="https://firebasestorage.googleapis.com/v0/b/zenda-186015.appspot.com/o/cms%2Fzendala%20landscape.png?alt=media&token=af50885a-7b33-474f-8a5f-15a114cf55f6"
-					/>
-					<meta
-						name="keywords"
-						content="aseguradora, seguros en línea, seguros online, seguro de auto, hospitalización, seguro médico"
-					/>
-				</Head>
+				<Head></Head>
 				<body>
 					<Main />
 					<NextScript />
@@ -79,6 +30,8 @@ class MyDocument extends Document {
 // 			});
 
 // 		const initialProps = await Document.getInitialProps(ctx);
+
+// 		console.log(initialProps);
 // 		return {
 // 			...initialProps,
 // 			styles: [
