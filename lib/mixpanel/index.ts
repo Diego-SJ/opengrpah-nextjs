95af5423f06df9053711da365a3da304d6123404
diego <diego@MBPPersdeMartin.bwrouter>
2022-04-23 14:01:55 -0500

    type errors fixed

--- lib/mixpanel/index.ts
+++ lib/mixpanel/index.ts
@@ -2,18 +2,18 @@ import mixpanelBrowser from 'mixpanel-browser';
 
 let mixpanelAvailable = false;
 
-if (mixpanelAvailable) mixpanelBrowser.init(process.env.MIXPANEL_TOKEN_ID);
+if (mixpanelAvailable) mixpanelBrowser.init(process.env.MIXPANEL_TOKEN_ID || '');
 
 let mixpanel = {
-  identify: (id) => {
-    if (mixpanelAvailable) mixpanelBrowser.identify(id);
-  },
-  alias: (id) => {
-    if (mixpanelAvailable) mixpanelBrowser.alias(id);
-  },
-  track: (name, props) => {
-    if (mixpanelAvailable) mixpanelBrowser.track(name, props);
-  },
+	identify: (id: string) => {
+		if (mixpanelAvailable) mixpanelBrowser.identify(id);
+	},
+	alias: (id: string) => {
+		if (mixpanelAvailable) mixpanelBrowser.alias(id);
+	},
+	track: (name: string, props: object) => {
+		if (mixpanelAvailable) mixpanelBrowser.track(name, props);
+	}
 };
 
 export default mixpanel;
--- lib/segment/index.ts
+++ lib/segment/index.ts
@@ -1,59 +1,59 @@
-import mixpanel from 'lib/mixpanel';
+import mixpanel from '../mixpanel';
 import { ElementColor, ElementType } from './index.types';
 
 const ACTIVATE_SEGMENT = false;
 
 export const button = {
-  clicked: async (label: string, color: ElementColor) => {
-    if (ACTIVATE_SEGMENT) {
-      mixpanel.track('Button Clicked', { label, color });
-      await window.analytics.track('Button Clicked', { label, color });
-    }
-  },
+	clicked: async (label: string, color: ElementColor) => {
+		if (ACTIVATE_SEGMENT) {
+			mixpanel.track('Button Clicked', { label, color });
+			await window.analytics.track('Button Clicked', { label, color });
+		}
+	}
 };
 
 export const link = {
-  clicked: async (label: string, url: string) => {
-    if (ACTIVATE_SEGMENT) {
-      mixpanel.track('Link Clicked', { label, url });
-      await window.analytics.track('Link Clicked', { label, url });
-    }
-  },
+	clicked: async (label: string, url: string) => {
+		if (ACTIVATE_SEGMENT) {
+			mixpanel.track('Link Clicked', { label, url });
+			await window.analytics.track('Link Clicked', { label, url });
+		}
+	}
 };
 
 export const element = {
-  focused: async (type: ElementType, label: string) => {
-    if (ACTIVATE_SEGMENT) {
-      await window.analytics.track('Element Focused', { type, label });
-      mixpanel.track('Element Focused', { type, label });
-    }
-  },
-  unfocused: async (type: ElementType, label: string) => {
-    if (ACTIVATE_SEGMENT) {
-      await window.analytics.track('Element Unfocused', { type, label });
-      mixpanel.track('Element Unfocused', { type, label });
-    }
-  },
-  closed: async (label: string, color: ElementColor) => {
-    if (ACTIVATE_SEGMENT) {
-      await window.analytics.track('Element Closed', { label, color });
-      mixpanel.track('Element Closed', { label, color });
-    }
-  },
+	focused: async (type: ElementType, label: string) => {
+		if (ACTIVATE_SEGMENT) {
+			await window.analytics.track('Element Focused', { type, label });
+			mixpanel.track('Element Focused', { type, label });
+		}
+	},
+	unfocused: async (type: ElementType, label: string) => {
+		if (ACTIVATE_SEGMENT) {
+			await window.analytics.track('Element Unfocused', { type, label });
+			mixpanel.track('Element Unfocused', { type, label });
+		}
+	},
+	closed: async (label: string, color: ElementColor) => {
+		if (ACTIVATE_SEGMENT) {
+			await window.analytics.track('Element Closed', { label, color });
+			mixpanel.track('Element Closed', { label, color });
+		}
+	}
 };
 
 export const page = (name: string, data?: {}) => {
-  if (ACTIVATE_SEGMENT) {
-    window?.analytics?.page(name, name, data || {});
-    mixpanel.track('Page Loaded', { url: name, ...data });
-  }
+	if (ACTIVATE_SEGMENT) {
+		window?.analytics?.page(name, name, data || {});
+		mixpanel.track('Page Loaded', { url: name, ...data });
+	}
 };
 
 export const identify = (id: string, data?: {}) => {
-  if (ACTIVATE_SEGMENT) {
-    window.analytics.identify(id, data);
-    mixpanel.identify(id);
-  }
+	if (ACTIVATE_SEGMENT) {
+		window.analytics.identify(id, data);
+		mixpanel.identify(id);
+	}
 };
 
 const segment = { element, link, button, page, identify };
