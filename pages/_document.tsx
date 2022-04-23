9fa80917d77db87698b84da42e8f7a74bb2ac51c
diego <diego@MBPPersdeMartin.bwrouter>
2022-04-23 14:41:12 -0500

    test 4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-// import { ServerStyleSheet } from 'styled-components';
-// import { ServerStyleSheets } from '@mui/styles';
+import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets } from '@mui/styles';
 
 class MyDocument extends Document {
 	render() {
@@ -17,34 +17,34 @@ class MyDocument extends Document {
 	}
 }
 
-// MyDocument.getInitialProps = async (ctx) => {
-// 	const sheet = new ServerStyleSheet();
-// 	const materialSheets = new ServerStyleSheets();
-// 	const originalRenderPage = ctx.renderPage;
+MyDocument.getInitialProps = async (ctx) => {
+	const sheet = new ServerStyleSheet();
+	const materialSheets = new ServerStyleSheets();
+	const originalRenderPage = ctx.renderPage;
 
-// 	try {
-// 		ctx.renderPage = () =>
-// 			originalRenderPage({
-// 				enhanceApp: (App) => (props) =>
-// 					sheet.collectStyles(materialSheets.collect(<App {...props} />))
-// 			});
+	try {
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App) => (props) =>
+					sheet.collectStyles(materialSheets.collect(<App {...props} />))
+			});
 
-// 		const initialProps = await Document.getInitialProps(ctx);
+		const initialProps = await Document.getInitialProps(ctx);
 
-// 		console.log(initialProps);
-// 		return {
-// 			...initialProps,
-// 			styles: [
-// 				<React.Fragment key="styles">
-// 					{initialProps.styles}
-// 					{sheet.getStyleElement()}
-// 					{materialSheets.getStyleElement()}
-// 				</React.Fragment>
-// 			]
-// 		};
-// 	} finally {
-// 		sheet.seal();
-// 	}
-// };
+		console.log(initialProps);
+		return {
+			...initialProps,
+			styles: [
+				<React.Fragment key="styles">
+					{initialProps.styles}
+					{sheet.getStyleElement()}
+					{materialSheets.getStyleElement()}
+				</React.Fragment>
+			]
+		};
+	} finally {
+		sheet.seal();
+	}
+};
 
 export default MyDocument;
