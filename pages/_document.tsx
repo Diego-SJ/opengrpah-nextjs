1940e9595a9e5f727d3098bd818f861e234a41c5
diego <diego@MBPPersdeMartin.bwrouter>
2022-04-23 14:52:02 -0500

    test 6

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,17 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en" dir="ltr">
-				<Head></Head>
+				<Head>
+					<meta charSet="utf-8" />
+					<meta name="facebook-domain-verification" content="409ftxgnl2ibsi2lit6lkeu2r5bgvn" />
+
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
