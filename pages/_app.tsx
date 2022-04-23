0f4fa0f75589ca248c60df3b5f9d6bb7b4be75d4
diego <diego@MBPPersdeMartin.bwrouter>
2022-04-23 14:55:54 -0500

    test 6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 import Head from 'next/head';
 // import GlobalThemeProvider from '../theme/theme';
 import App, { AppContext, AppProps } from 'next/app';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { Provider } from 'react-redux';
-// import store, { persistor } from '../redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import store, { persistor } from '../redux/store';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return (
-		<>
-			<Head>
-				<title>Next-Smrt</title>
-				<meta
-					name="viewport"
-					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-				/>
-			</Head>
-			<Component {...pageProps} />
-		</>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<>
+					<Head>
+						<title>Next-Smrt</title>
+						<meta
+							name="viewport"
+							content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+						/>
+					</Head>
+					<Component {...pageProps} />
+				</>
+			</PersistGate>
+		</Provider>
 	);
 };
 
